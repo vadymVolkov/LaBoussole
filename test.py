1fcb88ee8c31710a36db9769ae7eb80fb70437c7
Vadym Volkov <vadym@MacBook-Pro-Vadym.local>
2019-09-11 20:06:37 +0300

    add price

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from config import config
+import db
 
 ORDER_KEY = config.order_key
 def get_journals_from_docks():
-    # use creds to create a client to interact with the Google Drive API
-    scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('config/client_secret.json', scope)
-    client = gspread.authorize(creds)
-    # Find a workbook by key and open the first sheet
-    sheet = client.open_by_key(ORDER_KEY).get_worksheet(1)
-    result = sheet.get_all_values()
-    result = result[1:]
-    result1 = result[0][1]
-    result1 = result1.encode('ascii', 'ignore').decode('ascii')
-    return result1
-
+    journal_in_db = db.get_journal_by_name('Херсон')
+    return journal_in_db
 a = get_journals_from_docks()
 print(a)

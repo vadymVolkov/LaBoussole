8ee49b4ce741c0c66315a7efd2b4f8b0b1ea686a
Vadym Volkov <vadym@MacBook-Pro-Vadym.local>
2019-09-11 19:45:32 +0300

    add price

--- commands.py
+++ commands.py
@@ -317,8 +317,16 @@ def make_basket_record(user_id):
 
 
 def get_journals_from_docks():
-
-    result = 'test2'
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('config/client_secret.json', scope)
+    client = gspread.authorize(creds)
+    # Find a workbook by key and open the first sheet
+    sheet = client.open_by_key(ORDER_KEY).get_worksheet(1)
+    print(sheet)
+    result = sheet.get_all_values()
+    print(result)
+    result = result[1:]
     return result
 
 

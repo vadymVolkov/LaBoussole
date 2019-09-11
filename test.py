b414895342c8fa33c636bc5474042cfcca0db53e
Vadym Volkov <vadym@MacBook-Pro-Vadym.local>
2019-09-11 19:48:32 +0300

    add price

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-
+ORDER_KEY = config.order_key
 def get_journals_from_docks():
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds']
